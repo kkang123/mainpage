ca6cf0b18beb57b44c98208491661a9298098033
kkang123 <85389685+kkang123@users.noreply.github.com>
2022-12-12 13:21:44 +0900

    Update toggle.js

--- js/toggle.js
+++ js/toggle.js
@@ -1,5 +1,6 @@
 var check = $("input[type='checkbox']");
         check.click(function(){
-//             $(".item-photo").toggle();
-                 $("#img1").toggle();
+                'use strict';
+                
+             $(".item-photo").toggle();
         });
