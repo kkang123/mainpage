b5e9bb74fc58df59f9b865e12af31984f93db6de
kkang123 <85389685+kkang123@users.noreply.github.com>
2022-12-13 19:37:04 +0900

    Update toggle.js

--- js/toggle.js
+++ js/toggle.js
@@ -1,6 +1,13 @@
-var check = $("input[type='checkbox']");
-        check.click(function(){
-                'use strict';
+// var check = $("input[type='checkbox']");
+//         check.click(function(){
+//                 'use strict';
                 
-             $(".item-photo").toggle();
-        });
+//              $(".item-photo").toggle();
+//         });
+
+
+$(function (){
+	$("#btn_toggle").click(function (){
+  	$("#Toggle").toggle();
+  });
+});
